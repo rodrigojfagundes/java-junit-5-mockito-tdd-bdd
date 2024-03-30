f6feee0767c1ab9632ce64db87039d6cb69eba11
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-03-30 12:09:11 -0300

    Seção 4 - Parte 2

---
+++ S04_AdvancedConceptsofJUnit5/41 - TestMethodORder Ordenacao Randomica/S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTestS4.java
@@ -0,0 +1,97 @@
+package br.com.erudio.math;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+
+//Given -> Contexto/cenario inicial das classes, onde e setado as 
+//configurações, inicializar as var, etc...
+//When -> onde dispara o gatilho e inicia a execução do cenario
+//Then -> avaliação se o resultado corresponde ao esperado
+
+
+//classe q ira testar os METODOS da CLASSE SIMPLEMATHTEST
+//
+@DisplayName("Test Math Operations in Simple Math Class")
+class SimpleMathTestS4 {
+	
+	SimpleMath math;
+	
+	
+	@BeforeEach
+	void beforeEachMethod() {
+		math = new SimpleMath();
+	}
+	
+	
+	//criando o METODO TESTVALUESOURCE para criacao de TESTS PARAMETIZADOS
+	//
+	@ParameterizedTest
+	@ValueSource(strings = {"Pelé", "Senna", "Keith Moon"})
+	void testValueSource(String firsName) {
+		System.out.println(firsName);
+
+		assertNotNull(firsName);
+		
+	}
+	
+	
+	
+	//metodo para TESTAR o metodo DIVISION q ta na classe SIMPLEMATH
+	//
+	@DisplayName("Test double sbtraction [firstNumber, secondNumber, expected]")
+	@ParameterizedTest
+	//@MethodSource("testDivisionInputParameters")
+	//@MethodSource()
+	/**
+	@CsvSource({
+		"6.2, 2, 3.1",
+		"71, 14, 5.07",
+		"18.3, 3.1, 5.90", 
+	})
+	@CsvSource({
+	"Pele, Football",
+	"Senna, F1",
+	"Keith moon, ''"
+	})
+	*/
+	//usando a ANNOTATION @CSVFILESOURCE pois com ela e possivel ABRIR um ARQUIVO CSV
+	//e pegar o valor q ta nas LINHAS e usar esses valores como parametro...
+	//
+	@CsvFileSource(resources = "/testDivision.csv")
+	void testDivision(double firstNumber, double secondNumber, double expected) {
+		
+		System.out.println("Test " + firstNumber + "/" + 
+		secondNumber + " = " + expected + "!");
+		Double actual = math.division(firstNumber, secondNumber);
+		
+		assertEquals(expected, actual, 2D,
+				() -> firstNumber + "/" + secondNumber +"   not produce " + expected);
+	}
+	
+
+	/**
+	public static Stream<Arguments> testDivision() {
+		//dentro do STREAM vai ter ARGUMENTS com os valores 
+		//FIRSTNUMBER = 6.2D, SECONDNUMBER = 2D, EXPECTED = 3.1D... 
+		//e assim sucessivamente...
+		return Stream.of(
+				Arguments.of(6.2D, 2D, 3.1D),
+				Arguments.of(71D, 14D, 5.07D),
+				Arguments.of(18.3D, 3.1D, 5.90D)
+				);
+		
+	}
+	*/
+}
---
+++ S04_AdvancedConceptsofJUnit5/42 - @TestMethodOrder Ordenacao por nome/S04_AdvancedConceptsofJUnit5/src/test/java/br/com/erudio/math/SimpleMathTestS3.java
@@ -0,0 +1,202 @@
+package br.com.erudio.math;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+//Given -> Contexto/cenario inicial das classes, onde e setado as 
+//configurações, inicializar as var, etc...
+//When -> onde dispara o gatilho e inicia a execução do cenario
+//Then -> avaliação se o resultado corresponde ao esperado
+
+
+//classe q ira testar os METODOS da CLASSE SIMPLEMATHTEST
+//
+@DisplayName("Test Math Operations in Simple Math Class")
+class SimpleMathTestS3 {
+
+	SimpleMath math;
+	
+
+	//metodos q usam a ANNOTATION @BEFOREALL são exec antes de todos os TESTES da class
+	//
+	@BeforeAll
+	static void setup() {
+		System.out.println("Running @BEFOREALL method!");
+	}
+	
+
+	//metodos q usam a ANNOTATION @AFTERALL são exec no final de todos os TESTES da class
+	//
+	@AfterAll
+	static void clenup() {
+		System.out.println("Running @AFTERALL method!");
+	}
+	
+
+	//esse metodo com ANNOTATION @BeforeEach sera chamado automaticamente antes de exec cada
+	//metodo de teste
+	@BeforeEach
+	void beforeEachMethod() {
+		System.out.println("Running @BEFOREEACH method!");
+
+		math = new SimpleMath();
+	}
+	
+	
+	//metodos q usam ANNOTATION @AfterEach são exec a cada fez ter termina um METODO de TEST
+	//
+	@AfterEach
+	void afterEachMethod() {
+		System.out.println("Running @AFTEREACH method!");
+	}
+	
+	
+	//vamos testar o METODO SUM da classe SIMPLEMATH
+	//
+	@Test
+	@DisplayName("Test 6.2 + 2 = 8.2")
+	void testSum_When_SixDotTwoIsAddedByTwo_ShouldReturnEightDotTwo() {
+		
+		System.out.println("Test 6.2 + 2 = 8.2");
+
+		//Given/Arrange -> onde prepara e define as var q serao usadas no TEST
+
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		Double expected = 8.2D;
+
+		//When/Act -> QUANDO
+		Double actual = math.sum(firstNumber, secondNumber);
+
+		//Then/Assert -> Avaliacao se o resultado corresponde ao esperado
+		assertEquals(expected, actual, 
+				() -> firstNumber + "+" + secondNumber +"   not produce " + expected);
+
+	}
+	
+	
+	
+	//metodo para TESTAR o metodo SUBSTRACTION q ta na classe SIMPLEMATH
+	//
+	@Test
+	@DisplayName("Test 6.2 - 2 = 4.2")
+	void testSubtraction() {
+		
+		System.out.println("Test 6.2 - 2 = 4.2");
+
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		Double actual = math.subtraction(firstNumber, secondNumber);
+		Double expected = 4.2D;
+
+		assertEquals(expected, actual, 
+				() -> firstNumber + "-" + secondNumber +"   not produce " + expected);
+	}
+	
+	
+	
+	//metodo para TESTAR o metodo MULTIPLICATION q ta na classe SIMPLEMATH
+	//
+	@Test
+	@DisplayName("Test 6.2 * 2 = 12.4")
+	void testMultiplication() {
+		
+		System.out.println("Test 6.2 * 2 = 12.4");
+		
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		Double actual = math.multiplication(firstNumber, secondNumber);
+		Double expected = 12.4D;
+
+		assertEquals(expected, actual, 
+				() -> firstNumber + "*" + secondNumber +"   not produce " + expected);
+	}
+	
+	
+	
+	//metodo para TESTAR o metodo DIVISION q ta na classe SIMPLEMATH
+	//
+	@Test
+	@DisplayName("Test 6.2 / 2 = 3.1")
+	void testDivision() {
+		
+		System.out.println("Test 6.2 / 2 = 3.1");
+
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		Double actual = math.division(firstNumber, secondNumber);
+		Double expected = 3.1D;
+		
+		assertEquals(expected, actual, 
+				() -> firstNumber + "/" + secondNumber +"   not produce " + expected);
+	}
+	
+	
+	//testando o METODO DIVISION passando um valor de 0... Ou seja testando a divisao por ZERO
+	//ele deve retornar uma EXCEPTION ARITMETICA
+	@Test
+	@DisplayName("Test Division By Zero")
+	void testDivision_When_FirstNumberIsDividedByZero_ShouldThrowArithmeticException() {
+		
+		//GIVEN
+		double firstNumber = 6.2D;
+		double secondNumber = 0D;
+		var expectedMessage = "Impossible to divide by zero";
+		
+		//WHEN & THEN
+		ArithmeticException actual = assertThrows(ArithmeticException.class, () -> {
+		math.division(firstNumber, secondNumber);
+		}, () -> "Division by zero should throw an AritmeticException");
+
+		assertEquals(expectedMessage, actual.getMessage(),
+				() -> "Unexpected exception message!");
+		
+	}
+	
+	
+	//metodo para TESTAR o metodo MEAN q ta na classe SIMPLEMATH
+	//
+	@Test
+	@DisplayName("Test (6.2 + 2) / 2 = 4.1")
+	void testMean() {
+		
+		System.out.println("Test (6.2 + 2) / 2 = 4.1");
+
+		double firstNumber = 6.2D;
+		double secondNumber = 2D;
+		Double actual = math.mean(firstNumber, secondNumber);
+		Double expected = 4.1D;
+		
+		assertEquals(expected, actual, 
+				() -> "(" + firstNumber + "+" + secondNumber + ")/2" +
+				"did not produce " + expected + "!");
+	}
+	
+	
+	//metodo para TESTAR o metodo SQUAREROOT(RAIZ QUADRADA) q ta na classe SIMPLEMATH
+	//
+	@Test
+	@DisplayName("Test Square Root of 81 = 9")
+	void testSquareRoot() {
+		
+		System.out.println("Test 81 = 9");
+
+		double number = 81D;
+		Double expected = 9D;
+		Double actual = math.squareRoot(number);
+		
+		assertEquals(expected, actual, 
+				()-> "Square Root of" + number +
+				"did not produce " + expected + "!");
+	}
+	
+	
+}
