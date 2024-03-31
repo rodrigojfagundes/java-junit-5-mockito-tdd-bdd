ca9538bfd8ba825567fb20cbdaa809e4289ab0ef
Rodrigo Jose Fagundes <rodrigojosefagundes@gmail.com>
2024-03-30 21:11:33 -0300

    Seção 8 - Parte 3

---
+++ S08_CodeCoverage/89 - JaCoCo - Plugin Maven para Cobertura de Codigo/S08_CodeCoverage/src/test/java/br/com/erudio/mockito/HamcrestMatchersTest.java
@@ -0,0 +1,49 @@
+package br.com.erudio.mockito;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Test;
+
+class HamcrestMatchersTest {
+
+	@Test
+	void test() {
+		//GIVEN
+
+		//criando uma LISTA DE SCORE, com alguns valores
+		//do TIPO INTEGER
+		List<Integer> scores = Arrays.asList(99, 100, 101, 105);
+		
+		//WHEN & THEN
+		
+		//aqui nos estamos pegando a LISTA de nome SCORES e tamos vendo se o TAMANHO dela
+		//e 4
+		assertThat(scores, hasSize(4));
+
+		assertThat(scores, hasItems(100, 101));
+
+		assertThat(scores, everyItem(greaterThan(98)));
+
+		assertThat(scores, everyItem(lessThan(777)));
+		
+		assertThat("", is(emptyString()));
+
+		assertThat(null, is(emptyOrNullString()));
+
+		Integer[] myArray = {1, 2, 3};
+		
+		assertThat(myArray, arrayWithSize(3));
+		
+		assertThat(myArray, arrayContaining(1, 2, 3));
+		
+		assertThat(myArray, arrayContainingInAnyOrder(3, 2, 1));
+		
+		
+	}
+	
+}
